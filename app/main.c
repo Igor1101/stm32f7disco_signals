037ee33eb272cbfb0cdf2db93c7a239d998cf7eb
Igor <igor.muravyov.pz.2021@ukr.net>
2022-05-06 19:00:22 +0300

    add quantization of sin

--- app/main.c
+++ app/main.c
@@ -35,6 +35,10 @@
 volatile uint32_t RGB565_480x272[65280] = { 0x00000000 };
 const uint16_t green_color = 0x07E0;
 const uint16_t black_color = 0x0000;
+const uint16_t brown_color = 0x8160;
+const uint16_t orange_color = 0xFB20;
+const uint16_t yellow_color = 0xFD00;
+const uint16_t red_color = 0xF800;
 
 int exp_data(void);
 void task_app0(void const * argument)
@@ -89,12 +93,11 @@ void lcd_diag_item(uint16_t offset, int item, uint16_t color)
 	lcd_col(offset, color, vert_start, vert_end);
 }
 
-void lcd_diagram(int*arr, size_t sz)
+void lcd_diagram(int offs, int*arr, size_t sz, const uint16_t color)
 {
 
 	uint32_t h = hltdc.LayerCfg[0].ImageHeight;
 	uint32_t w = hltdc.LayerCfg[0].ImageWidth;
-	lcd_background(black_color);
 	// find max in arr
 	int i, max, min;
 	for(max = arr[0], min = arr[0], i=0; i<sz; i++) {
@@ -103,10 +106,16 @@ void lcd_diagram(int*arr, size_t sz)
 		if(arr[i] < min)
 			min  = arr[i];
 	}
-	int nmax = (abs(min) > abs(max))?abs(min):abs(max);
+	static uint8_t firsttime=1;
+	static int nmax;
+	if(firsttime) {
+		nmax = (abs(min) > abs(max))?abs(min):abs(max);
+		firsttime = 0;
+	}
 	for(int i=0; i<sz; i++) {
 		int rec_item = (int)h * arr[i] / nmax /2;
-		lcd_diag_item(i*10, rec_item, green_color);
+		lcd_diag_item(offs+i*10, rec_item, color);
+		lcd_diag_item(offs+1+i*10, rec_item, color);
 	}
 }
 int    Ai[8];
@@ -133,19 +142,23 @@ void app_main(void) {
 	_expb_2();
 	result1= _expb_3(Ai, Xi);
 	result2 = _expb_4(Ai, Xi);
-	//exp_data();
+	exp_data();
 	__enable_irq();
 }
 
 #define BUF_SIZE    40
 const int sineTable[BUF_SIZE]=
-    {0x0000,0x000f,0x001e,0x002d,0x003a,0x0046,0x0050,0x0059,
-     0x005f,0x0062,0x0063,0x0062,0x005f,0x0059,0x0050,0x0046,
-     0x003a,0x002d,0x001e,0x000f,0x0000,0xfff1,0xffe2,0xffd3,
-     0xffc6,0xffba,0xffb0,0xffa7,0xffa1,0xff9e,0xff9d,0xff9e,
-     0xffa1,0xffa7,0xffb0,0xffba,0xffc6,0xffd3,0xffe2,0xfff1};
+  {0x0000,0x01E0,0x03C0,0x05A0,0x0740,0x08C0,0x0A00,0x0B20,
+   0x0BE0,0x0C40,0x0C60,0x0C40,0x0BE0,0x0B20,0x0A00,0x08C0,
+   0x0740,0x05A0,0x03C0,0x01E0,0x0000,0xFE20,0xFC40,0xFA60,
+   0xF8C0,0xF740,0xF600,0xF4E0,0xF420,0xF3C0,0xF3A0,0xF3C0,
+   0xF420,0xF4E0,0xF600,0xF740,0xF8C0,0xFA60,0xFC40,0x0000};
 int in_buffer[BUF_SIZE];
 int out_buffer[BUF_SIZE];
+int out16[BUF_SIZE];   	/* 16 bits output sample buffer */
+int out12[BUF_SIZE];    /* 12 bits output sample buffer */
+int out8[BUF_SIZE];     /* 8 bits output sample buffer */
+int out6[BUF_SIZE];     /* 6 bits output sample buffer */
 int Gain, Noise,Ac;
 static int arr[5] = {-1, 3,-4,8, -7};
 int exp_data(void)
@@ -153,7 +166,7 @@ int exp_data(void)
 
     int i,j;
 
-    lcd_diagram(arr, sizeof arr / sizeof(int));
+    //lcd_diagram(arr, sizeof arr / sizeof(int));
     Gain = 0x20;
     Noise = 580;
     Ac=0;
@@ -165,18 +178,32 @@ int exp_data(void)
         {
             j = BUF_SIZE-1-i;
             out_buffer[j] = 0;
+            out16[j] = 0;
+            out12[j] = 0;
+            out8[j] = 0;
+            out6[j] = 0;
             in_buffer[j] = 0;
         }
         for (i = BUF_SIZE-1; i>= 0; i--)
         {
             j = BUF_SIZE-1-i;
-            in_buffer[i] = (short int)sineTable[i];/* set breakpoint */
-            in_buffer[i] = 0 - in_buffer[i];
-            out_buffer[j] = Gain*in_buffer[i] + (rand() / (float)RAND_MAX * Noise)+Ac;
+            out_buffer[j] = Gain*(0-(short int)sineTable[i]) + Ac;
+            out16[i] = Gain*((short int)sineTable[i])+Ac; 		/* 16-bit data */
+            out12[i] = Gain*(((short int)(sineTable[i])&0xfff0))+Ac; /* Mask off 4-bit*/
+            out8[i] = Gain*(((short int)(sineTable[i])&0xff00))+Ac; 	/* Mask off 8-bit*/
+            out6[i] = Gain*((((short int)sineTable[i])&0xfc00))+Ac; 	/* Mask off 10-bit*/
         }
-
-            lcd_diagram(out_buffer, sizeof(out_buffer)/ sizeof(int));
-
+    	lcd_background(black_color);
+        lcd_diagram(0, out_buffer, sizeof(out_buffer)/ sizeof(int), green_color);
+        osDelay(1000);
+        lcd_diagram(2,out16, sizeof(out_buffer)/ sizeof(int), yellow_color);
+        osDelay(1000);
+        lcd_diagram(4,out12, sizeof(out_buffer)/ sizeof(int), red_color);
+        osDelay(1000);
+        lcd_diagram(6,out8, sizeof(out_buffer)/ sizeof(int), brown_color);
+        osDelay(1000);
+        lcd_diagram(8,out6, sizeof(out_buffer)/ sizeof(int), orange_color);
+        osDelay(1000);
     }   /* <- set probe and profile point on this line */
 
 }
